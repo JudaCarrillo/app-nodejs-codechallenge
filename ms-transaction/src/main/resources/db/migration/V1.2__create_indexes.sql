c63ce410feddabd9aac3747612c30aad64a68849
Juda Carrillo <judacarrillot@gmail.com>
2026-01-30 13:42:40 -0500

    Revert "Feature/implement create transaction"

--- ms-transaction/src/main/resources/db/migration/V1.2__create_indexes.sql
+++
@@ -1,5 +0,0 @@
-CREATE INDEX IF NOT EXISTS idx_transaction_status_code ON transaction_status (code);
-
-CREATE INDEX IF NOT EXISTS idx_transfer_type_id ON transfer_type (transfer_type_id);
-
-CREATE INDEX IF NOT EXISTS idx_transaction_external_id ON transaction (transaction_external_id);
